b9a5a6917cea9827d97fc6612517ce022db6e4a9
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-09 18:35:20 +0300

    Create day-04.js

---
+++ week-01/day-04.js
@@ -0,0 +1,48 @@
+//https://www.codewars.com/kata/57216d4bcdd71175d6000560/train/javascript
+
+function padIt(str, n) {
+  while (n--) {
+    str = n % 2 === 0 ? "*" + str : str + "*";
+  }
+  return str;
+}
+
+
+//https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+
+function pickIt(arr) {
+  const odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else odd.push(arr[i]);
+  }
+  return [odd, even];
+}
+
+
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
+
+function grabDoll(dolls) {
+  let bag = [],
+    doll;
+  for (doll of dolls) {
+    if (doll === "Hello Kitty" || doll === "Barbie doll") bag.push(doll);
+    else continue;
+    if (bag.length > 2) break;
+  }
+  return bag;
+}
+
+
+//https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
+
+function giveMeFive(obj) {
+  let five = [];
+  for (var key in obj) {
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]);
+  }
+  return five;
+}
