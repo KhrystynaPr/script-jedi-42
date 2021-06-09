be4fc52dd7706bb1056984d3116b2fcb6b051278
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-09 18:52:25 +0300

    Update day-05.js

--- week-01/day-05.js
+++ week-01/day-05.js
@@ -7,3 +7,9 @@ function correctTail(body, tail) {
 
 //https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
 
+function bonusTime(salary, bonus) {
+  return bonus ? "£" + salary * 10 : "£" + salary;
+}
+
+
+
