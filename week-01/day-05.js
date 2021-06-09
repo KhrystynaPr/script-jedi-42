2ab2abb37ec7c5446f4e061e70235c448168d150
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-09 18:48:23 +0300

    Create day-05.js

---
+++ week-01/day-05.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/is-this-my-tail/train/javascript
+
+function correctTail(body, tail) {
+  return body[body.length - 1] === tail;
+}
+
+
+//https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
