2b2d4875842e058951e8e36547822acbd5a2b13a
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-09 19:42:04 +0300

    Update day-05.js

--- week-01/day-05.js
+++ week-01/day-05.js
@@ -12,4 +12,12 @@ function bonusTime(salary, bonus) {
 }
 
 
+//https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
+
+function automorphic(n) {
+  const sqString = Math.pow(n, 2).toString();
+  return sqString.endsWith(n.toString()) ? "Automorphic" : "Not!!";
+}
+
+
 
