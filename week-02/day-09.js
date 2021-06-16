bffa1ee8ef3a25d3cbfe04043fe6ede8ea86b0cb
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-16 19:37:23 +0300

    Create day-09.js

---
+++ week-02/day-09.js
@@ -0,0 +1,29 @@
+//https://www.codewars.com/kata/57274562c8dcebe77e001012
+
+function cutIt(arr) {
+  let minLength = arr[0].length;
+  arr.forEach(function (str) {
+    if (str.length < minLength) {
+      minLength = str.length;
+    }
+  });
+  return arr.map((elem) => elem.slice(0, minLength));
+}
+
+
+//https://www.codewars.com/kata/57277a31e5e51450a4000010
+
+function firstToLast(str, c) {
+  const firstIndex = str.search(c);
+  return firstIndex < 0 ? -1 : str.lastIndexOf(c) - firstIndex;
+}
+
+
+//https://www.codewars.com/kata/57280481e8118511f7000ffa
+
+function splitAndMerge(string, separator) {
+  return string
+    .split(" ")
+    .map((element) => element.split("").join(separator))
+    .join(" ");
+}
