c76672cfc4af0c3ff3684fc079296e812558f2ce
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-23 21:37:06 +0300

    Update day-10.js

--- week-02/day-10.js
+++ week-02/day-10.js
@@ -2,13 +2,12 @@
 
 function alienLanguage(str) {
   const lowerCaseArray = str.toLowerCase().split(" ");
-  const modifiedArray = lowerCaseArray.map((elem) => {
+  const modifiedArray = lowerCaseArray.map(elem => {
     const stringToReplace = elem.substr(0, elem.length - 1);
     return elem.replace(stringToReplace, stringToReplace.toUpperCase());
   });
   return modifiedArray.join(" ");
 }
-
 
 
 //https://www.codewars.com/kata/57284d23e81185ae6200162a
@@ -23,4 +22,47 @@ function fiveLine(s) {
 }
 
 
-//
+//https://www.codewars.com/kata/57284d23e81185ae6200162a/train/javascript
+
+function topSecret(str) {
+  const decryptedText = str
+    .split("")
+    .map((char) => decrypt(char))
+    .join("");
+  // getFirstAnswer(decryptedText)
+  // getSecondAnswer(decryptedText)
+  // getThirdAnswer(decryptedText)
+  return decryptedText;
+}
+
+// function getFirstAnswer(decryptedText) {
+// }
+
+// function getSecondAnswer(decryptedText) {
+// }
+
+// function getThirdAnswer(decryptedText) {
+// }
+
+function decrypt(char) {
+  const charCode = char.charCodeAt();
+  if (!(isUpperCaseLetter(charCode) || isLowerCaseLetter(charCode))) {
+    return char;
+  }
+  const newCharCode = charCode - 3;
+  if (newCharCode < 65 && isUpperCaseLetter(charCode)) {
+    return String.fromCharCode(91 - (65 - newCharCode));
+  }
+  if (newCharCode < 97 && isLowerCaseLetter(charCode)) {
+    return String.fromCharCode(123 - (97 - newCharCode));
+  }
+  return String.fromCharCode(newCharCode);
+}
+
+function isLowerCaseLetter(charCode) {
+  return charCode >= 97 && charCode <= 122;
+}
+
+function isUpperCaseLetter(charCode) {
+  return charCode >= 65 && charCode <= 90;
+}
