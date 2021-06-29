6691747c4d8bbaf472069fdfb5aab2620a854965
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-29 21:50:39 +0300

    Create day-16.js

---
+++ week-03/day-16.js
@@ -0,0 +1,47 @@
+//https://www.codewars.com/kata/572cb264362806af46000793 
+
+function threeInOne(arr) {
+  const newArray = arr.slice();
+  const modifiedArray = [];
+  while (newArray.length > 0) {
+    modifiedArray.push(newArray.splice(0, 3));
+  }
+  return modifiedArray.map((item) => item.reduce((a, b) => a + b));
+}
+
+
+//https://www.codewars.com/kata/572df796914b5ba27c000c90
+
+function sortIt(arr) {
+  const modifiedArray = arr.slice();
+  const repeats = {};
+  modifiedArray.forEach((elem) =>
+    !repeats[elem] ? (repeats[elem] = 1) : (repeats[elem] += 1)
+  );
+  modifiedArray.sort((a, b) => {
+    if (repeats[a] > repeats[b]) {
+      return 1;
+    }
+    if (repeats[a] == repeats[b]) {
+      return b - a;
+    }
+    return -1;
+  });
+  return modifiedArray;
+}
+
+ 
+//https://www.codewars.com/kata/572fdeb4380bb703fc00002c перевірити чи не міняється вхідний масив
+
+function isolateIt(arr){
+ return arr.map(item => { 
+    if (item.length%2===0) {
+      let subArray = item.split("") 
+        subArray.splice(item.length / 2, 0, "|")
+        return subArray.join("")  
+    }
+    let subArray = item.split("") 
+        subArray.splice(item.length / 2, 1, "|")
+        return subArray.join("")  
+  })
+}
