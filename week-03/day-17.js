1b46269ac11d5b7bbf6907d3d7648d712041bd8b
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-06-30 20:19:42 +0300

    Update day-17.js

--- week-03/day-17.js
+++ week-03/day-17.js
@@ -42,7 +42,7 @@ function reverseEqual(a, b) {
 //https://www.codewars.com/kata/5731861d05d14d6f50000626
 
 function bigToSmall(arr) {
-  let totalArray = [];
+  const totalArray = [];
   arr.forEach((elem) => totalArray.push(...elem));
   totalArray.sort((a, b) => b - a);
   return totalArray.join(">");
