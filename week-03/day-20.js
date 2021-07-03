50483dc30af816b6427bbc08184c1ed65592b38d
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-03 12:53:48 +0300

    Create day-20.js

---
+++ week-03/day-20.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/573bca07dffc1aa693000139
+const regex = /^-?9{1}.*0{4}$/;
+
+
+//https://www.codewars.com/kata/573d11c48b97c0ad970002d4
+const regex = /(http:|https:)\/{2}[a-z0-9.]+([.]net|[.]com)+/gi;
