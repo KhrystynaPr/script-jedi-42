d55e74895844ce9af952da01557e4251aeb0863e
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-04 12:38:02 +0300

    Update day-21.js

--- week-03/day-21.js
+++ week-03/day-21.js
@@ -1,2 +1,15 @@
-//https://www.codewars.com/kata/573e6831e3201f6a9b000971
+// https://www.codewars.com/kata/573e6831e3201f6a9b000971
 const regex = /\b(\w)(\w)?(\w)?\w?\3\2\1\b/g;
+
+
+// https://www.codewars.com/kata/573fb9223f9793e485000453
+
+const regex = /\d{3}(?!\$)/g;
+function addCommas(money, reg) {
+  const reversedStr = reverse(money).replace(reg, (x) => x + ",");
+  return reverse(reversedStr);
+}
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
