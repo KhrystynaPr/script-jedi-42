f4d7456029ee6a966730e671bb2fe64ccca6be25
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-06 13:28:46 +0300

    Create day-24.js

---
+++ week-04/day-24.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/520d9c27e9940532eb00018e
+
+function solution(...args) {
+  const set = new Set(args);
+  return set.size !== args.length;
+}
+
+
+// https://www.codewars.com/kata/541629460b198da04e000bb9
+
+function last() {
+  const lastArg = arguments[arguments.length - 1];
+  if (Array.isArray(lastArg) || typeof lastArg === "string") {
+    return lastArg[lastArg.length - 1];
+  }
+  return lastArg;
+}
