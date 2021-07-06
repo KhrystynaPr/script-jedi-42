bc77295030fb1a2306f17ad03d620785eed3d330
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-06 13:32:44 +0300

    Update day-24.js

--- week-04/day-24.js
+++ week-04/day-24.js
@@ -8,7 +8,7 @@ function solution(...args) {
 
 // https://www.codewars.com/kata/541629460b198da04e000bb9
 
-function last() {
+function last(list) {
   const lastArg = arguments[arguments.length - 1];
   if (Array.isArray(lastArg) || typeof lastArg === "string") {
     return lastArg[lastArg.length - 1];
