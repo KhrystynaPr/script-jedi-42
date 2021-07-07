766d5c9ae29d902fab2082e5d83480b0b96000dd
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-07 17:36:13 +0300

    Create day-25.js

---
+++ week-04/day-25.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/52d56d9c6b02b2fa9a0006d9
+
+Function.prototype.call = function() {
+  const arrayOfArgs = Array.from(arguments).slice(1);
+  return this.apply(arguments[0], arrayOfArgs);
+};
+
+
+// https://www.codewars.com/kata/53d4eec01f1a9b3020000786
+
+name = 'The Window';
+const alpha = {
+    name : 'My Alpha',
+    getNameFunc : function() {
+        return function() {
+            return this.name;
+        }.bind(alpha);
+    }
+};
