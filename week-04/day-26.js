9f94954b012824883a7d82b9ef42bdd663ae9de2
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-09 17:32:35 +0300

    Update day-26.js

--- week-04/day-26.js
+++ week-04/day-26.js
@@ -1,4 +1,12 @@
-//
+// https://www.codewars.com/kata/5388a9d60b24c52f4c000b5f
+
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function(...args) {
+    const newCtx = this === global ? ctx : this;
+    return func.apply(newCtx, args);
+  };
+};
 
 
 
