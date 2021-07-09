52e381e673b70422926a32db3af55926026cc9ad
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-09 14:59:52 +0300

    Update day-28.js

--- week-04/day-28.js
+++ week-04/day-28.js
@@ -5,4 +5,12 @@ function always (n) {
 }
 
 
-// 
+// https://www.codewars.com/kata/59bdbe9d46038724ca0000b9
+
+const state = {
+  modifier: 2,
+};
+
+function solution(arr, options) { 
+  return arr.map(el => el + 2 * options.modifier);  
+}
