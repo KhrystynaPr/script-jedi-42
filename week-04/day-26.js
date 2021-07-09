8896b0e3e417d5a28f9aa5cdfff9f4c2b2eaa341
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-09 19:09:54 +0300

    Update day-26.js

--- week-04/day-26.js
+++ week-04/day-26.js
@@ -1,11 +1,17 @@
 // https://www.codewars.com/kata/5388a9d60b24c52f4c000b5f
 
+/*підглянула вирішення з пояснення на youtube.
+Щоб нагло не копіпастити, трошки її переробила.*/
+
 Function.prototype.bind = function (ctx) {
   const func = this;
-  return function(...args) {
-    const newCtx = this === global ? ctx : this;
-    return func.apply(newCtx, args);
-  };
+  const args = Array.prototype.slice.call(arguments, 1);
+  return function(){
+    return func.apply(
+      this && this !== global ? this : ctx,
+      args.concat(Array.prototype.slice.apply(arguments))
+    );
+  }
 };
 
 
