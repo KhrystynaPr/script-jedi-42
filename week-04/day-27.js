8cddb9f5f71584e880f4305eb4481a24f6526f75
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-09 14:12:36 +0300

    Create day-27.js

---
+++ week-04/day-27.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+
+// https://www.codewars.com/kata/514aa0dc21607ae236000017
+
+let name;
+function greet_abe() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+
+function greet_ben() {
+  name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
