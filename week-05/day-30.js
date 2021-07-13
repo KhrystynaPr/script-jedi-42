69b6b229b06c9b29a818ddddfb1b4f4f373f08b5
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-13 19:44:19 +0300

    Create day-30.js

---
+++ week-05/day-30.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/santaclausable-interface
+
+function isSantaClausable(obj) {
+  const arrayOfMethods = ["sayHoHoHo", "distributeGifts", "goDownTheChimney"];
+  return arrayOfMethods.every((elem) => typeof obj[elem] === "function");
+}
+
+
+// https://www.codewars.com/kata/cylon-evolution
+
+class Cylon {
+  constructor(model) {
+    this.model = model;
+    this.attack = () => `Destroy all humans!`;
+  }
+}
+
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model);
+    this.infiltrate = () => `Infiltrate the colonies`;
+  }
+}
