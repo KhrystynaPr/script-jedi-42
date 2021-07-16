792efa191c212c443c0ad92c7e66125e186419fb
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-16 22:42:27 +0300

    Create day-33.js

---
+++ week-05/day-33.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/array-number-reduce/train/javascript
+
+Array.prototype.reduce = function (process, initial) {
+  for (const elem of this) {
+    if (!initial) {
+      initial = elem;
+      continue;
+    }
+    initial = process(initial, elem);
+  }
+  return initial;
+};
+
+
+// https://www.codewars.com/kata/52ed80dfe7bf9ae61d000056/train/javascript
+
+Object.deepFreeze = function (object) {
+  for (const key of Object.keys(object)) {
+    object[key] = Object.deepFreeze(object[key]);
+  }
+  return Object.freeze(object);
+};
