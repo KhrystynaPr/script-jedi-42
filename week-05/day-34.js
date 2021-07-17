8463c7b42ac4347e11f09d5dc5c8f915cdf0e785
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-17 19:42:52 +0300

    Create day-34.js

---
+++ week-05/day-34.js
@@ -0,0 +1,36 @@
+// https://www.codewars.com/kata/547274e24481cfc469000416
+
+class God {
+  static create() {
+    return [new Man(), new Woman()];
+  }
+}
+
+class Human {}
+
+class Man extends Human {}
+
+class Woman extends Human {}
+
+
+// https://www.codewars.com/kata/56ff9b53140fcca90b000530
+
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super(name, age, gender, "Labrador", "Large", master, true);
+  }
+}
+
+
+// https://www.codewars.com/kata/55a144eff5124e546400005a
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
