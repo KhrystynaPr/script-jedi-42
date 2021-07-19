3e4267dc54fc9f821e6830e692f481bce67b7579
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-19 21:16:54 +0300

    Update day-35.js

--- week-05/day-35.js
+++ week-05/day-35.js
@@ -1,6 +1,8 @@
 // https://www.codewars.com/kata/557e508a47c7e9adf9000062
 
-Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
+[gadget, widget, thingamabob].forEach(function(item) {
+  item.__proto__.description = "TODO: GIVE ME A DESCRIPTION";
+});
 
 
 // https://www.codewars.com/kata/513e1e47c600c93cef000001/train/javascript
