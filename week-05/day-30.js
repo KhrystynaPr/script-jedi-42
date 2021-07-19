366c58b248bc67934b324da84e7ba33bb70db798
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-19 21:16:46 +0300

    Update day-30.js

--- week-05/day-30.js
+++ week-05/day-30.js
@@ -10,15 +10,19 @@ function isSantaClausable(obj) {
 
 class Cylon {
   constructor(model) {
-    this.model = model;
-    this.attack = () => `Destroy all humans!`;
+    this.model = model; 
   }
+  attack() {
+   return `Destroy all humans!`;
+  } 
 }
 
 class HumanSkin extends Cylon {
   constructor(model) {
-    super(model);
-    this.infiltrate =  () => `Infiltrate the colonies`;
+    super(model); 
+  }
+  infiltrate() {
+    return `Infiltrate the colonies`;
   }
 }
 
