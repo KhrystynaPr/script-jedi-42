9d2bfc664ca89d93b502044e96db99527b543b82
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-20 19:51:40 +0300

    Update day-36.js

--- week-06/day-36.js
+++ week-06/day-36.js
@@ -3,19 +3,13 @@
 function submitOrder(user) {
   let shoppingCart, zipCode, shippingRate, orderSuccessful;
 
-  const shoppingCartPromise = OrderAPI.getShoppingCartAsync(user).then(function (cart) {
-      shoppingCart = cart;
-    }
-  );
-
-  const profilePromise = CustomerAPI.getProfileAsync(user).then(function (profile) {
-    zipCode = profile.zipCode;
-  });
-
-  Promise.all([shoppingCartPromise, profilePromise]).then(function () {
+  Promise.all([
+    OrderAPI.getShoppingCartAsync(user).then((cart) => (shoppingCart = cart)),
+    CustomerAPI.getProfileAsync(user).then((profile) => (zipCode = profile.zipCode)),
+  ]).then(function () {
     shippingRate = calculateShipping(shoppingCart, zipCode);
 
-    OrderAPI.placeOrderAsync(shoppingCart, shippingRate).then(function (success) {
+    OrderAPI.placeOrderAsync(shoppingCart, shippingRate).then((success) => {
       orderSuccessful = success;
       console.log(`Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`);
     });
@@ -25,14 +19,11 @@ function submitOrder(user) {
 // implement with async/await
 
 async function submitOrder(user) {
-  let shoppingCart, zipCode, shippingRate, orderSuccessful;
   
-  await OrderAPI.getShoppingCartAsync(user).then(cart => shoppingCart = cart);
-  await CustomerAPI.getProfileAsync(user).then(profile => zipCode = profile.zipCode);
-
-  shippingRate = calculateShipping(shoppingCart, zipCode);
-
-  await OrderAPI.placeOrderAsync(shoppingCart, shippingRate).then(success => orderSuccessful = success);
+  const shoppingCart = await OrderAPI.getShoppingCartAsync(user);
+  const { zipCode } = await CustomerAPI.getProfileAsync(user);
+  const shippingRate = calculateShipping(shoppingCart, zipCode);
+  const orderSuccessful = await OrderAPI.placeOrderAsync(shoppingCart, shippingRate);
   
   console.log(`Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`);
 }
