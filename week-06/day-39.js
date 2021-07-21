4608610b8843db6bff93bc2f8edef02f062c6f16
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-07-21 23:02:57 +0300

    Update day-39.js

--- week-06/day-39.js
+++ week-06/day-39.js
@@ -3,7 +3,7 @@
 function* generator(a) {
   let count = 1;
   while (true) {
-    const arg = yield `${a} x ${count} = ${a * count}`;
+    yield `${a} x ${count} = ${a * count}`;
     count++;
   }
 }
